23826104ac08cfadd34b21d6f70aa51df6d04ce2
hemavathi-github <75414397+hemavathi-github@users.noreply.github.com>
2024-06-21 15:08:44 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -7,30 +7,24 @@ var peer = new Peer(undefined, {
 });
 
 const user = prompt("Enter your name");
+const myvideo=document.createElement("video")
+myvideo.muted=true
 
-const myVideo = document.createElement("video");
-myVideo.muted = true;
-
-let myStream;
-
-navigator.mediaDevices
-    .getUserMedia({
-        audio: true,
-        video: true,
-    })
-    .then((stream) => {
-        myStream = stream;
-        addVideoStream(myVideo, stream);
-    })
-
-function addVideoStream(video, stream) {
-    video.srcObject = stream;
-    video.addEventListener("loadedmetadata", () => {
-        video.play();
+var mystream
+navigator.mediaDevices.getUserMedia({
+    audio:true,video:true
+}).then((streamingdata)=>{
+    mystream=streamingdata
+    videostreaming(myvideo,streamingdata)
+})
+function videostreaming(video,streamingdata){
+    video.srcObject=streamingdata
+    video.addEventListener("loadedmedia",()=>{
+        video.play()
         $("#video_grid").append(video)
-    });
-};
+    })
 
+}
 $(function () {
     $("#show_chat").click(function () {
         $(".left-window").css("display", "none")
